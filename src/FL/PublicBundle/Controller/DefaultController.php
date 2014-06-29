5533a4e2e3614173cadcf36ceb2f163d43b614d6
Florian <florian.lepot@gmail.com>
2014-06-29 22:40:17 +0200

    Add contact form and process

--- src/FL/PublicBundle/Controller/DefaultController.php
+++ src/FL/PublicBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace FL\PublicBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -21,8 +22,26 @@ class DefaultController extends Controller
      * @Route("/contact", name="contact")
      * @Template()
      */
-    public function contactAction()
+    public function contactAction(Request $request)
     {
+        if ($request->getMethod() == 'POST') {
+            $name = $request->request->get('name');
+            $email = $request->request->get('email');
+            $title = $request->request->get('title');
+            $message = $request->request->get('message');
+
+            $message = \Swift_Message::newInstance()
+                ->setSubject('[Portfolio] ' . $title)
+                ->setFrom($email)
+                ->setTo('florian.lepot@gmail.com')
+                ->setContentType('text/html')
+                ->setBody($this->renderView('FLPublicBundle:Mail:contact.html.twig', array('name' => $name, 'message' => $message)));
+            ;
+            $this->get('mailer')->send($message);
+
+            return array('message' => '<i class="fa fa-ok"></i> Votre message a bien été envoyé, vous aurez une réponse très prochainement !');
+        }
+
     	return array();
     }
 
